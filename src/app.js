c2936b3bce8e640fda8f7db7a55be23d09db7cc0
jutionck <jutionck@gmail.com>
2023-05-07 10:35:50 +0700

    add: create element multiple

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading1 = React.createElement("h1", {
-  children: "Biodata Perusahaan",
-  className: "heading-1",
-});
-const heading2 = React.createElement("h2", {
-  children: "PT Enigma Cipta Humanika",
-});
+// const heading1 = React.createElement("h1", {
+//   children: "Biodata Perusahaan",
+//   className: "heading-1",
+// });
+// const heading2 = React.createElement("h2", {
+//   children: "PT Enigma Cipta Humanika",
+// });
 
-const element = React.createElement(React.Fragment, {
-  children: [heading1, heading2],
-});
+const element = React.createElement(
+  "ul",
+  null,
+  React.createElement("li", null, "Mangga"),
+  React.createElement("li", null, "Jeruk"),
+  React.createElement("li", null, "Pisang")
+);
 
 const root = createRoot(document.getElementById("root"));
 
